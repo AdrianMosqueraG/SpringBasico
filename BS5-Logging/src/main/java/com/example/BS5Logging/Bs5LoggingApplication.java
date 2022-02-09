717831246cca205d2d409a37e093021fa2d4f530
AdrianMosqueraG <79195289+AdrianMosqueraG@users.noreply.github.com>
2022-02-09 10:04:59 +0100

    Add files via upload

---
+++ BS5-Logging/src/main/java/com/example/BS5Logging/Bs5LoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.BS5Logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bs5LoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bs5LoggingApplication.class, args);
+	}
+
+}
---
+++ BS5-Logging/src/test/java/com/example/BS5Logging/Bs5LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BS5Logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bs5LoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
