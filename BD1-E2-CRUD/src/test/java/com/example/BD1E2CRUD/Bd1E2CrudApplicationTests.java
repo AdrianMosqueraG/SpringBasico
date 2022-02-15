dd7cfd7306c94a2b23407c00dbad5fd4e8d2e6ea
AdrianMosqueraG <79195289+AdrianMosqueraG@users.noreply.github.com>
2022-02-15 16:57:57 +0100

    Add files via upload

---
+++ BD1-E2-CRUD/src/test/java/com/example/BD1E2CRUD/Bd1E2CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BD1E2CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bd1E2CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
