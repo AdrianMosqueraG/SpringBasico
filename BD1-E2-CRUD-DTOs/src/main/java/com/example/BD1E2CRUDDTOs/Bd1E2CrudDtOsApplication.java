91aaae93480a1ddd2afc1f3fce38c0a2280e9281
AdrianMosqueraG <79195289+AdrianMosqueraG@users.noreply.github.com>
2022-02-17 17:09:19 +0100

    Add files via upload

---
+++ BD1-E2-CRUD-DTOs/src/main/java/com/example/BD1E2CRUDDTOs/Bd1E2CrudDtOsApplication.java
@@ -0,0 +1,13 @@
+package com.example.BD1E2CRUDDTOs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bd1E2CrudDtOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bd1E2CrudDtOsApplication.class, args);
+	}
+
+}
---
+++ BD1-E2-CRUD-DTOs/src/test/java/com/example/BD1E2CRUDDTOs/Bd1E2CrudDtOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BD1E2CRUDDTOs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bd1E2CrudDtOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
